1d16cb2725a70226901a88127d7d2d844038271b
marcosherna <marcosxdf6@gmail.com>
2024-04-06 12:27:49 -0600

    validacion al modelo grupo

--- models/Grupo.cs
+++ models/Grupo.cs
@@ -9,12 +9,20 @@ using System.Threading.Tasks;
 namespace guia_2.models
 {
     public class Grupo: Entity {
+
+        [Required(ErrorMessage = "El campo es requerido")]
         public string IdCarrera { get; set; } = "";
+
+        [Required(ErrorMessage = "El campo es requerido")]
         public string IdMateria { get; set; } ="";
+
+        [Required(ErrorMessage = "El campo es requerido")]
         public string IdProfesor { get; set; } = "";
         
         [Required(ErrorMessage = "El campo es requerido")]
         public int Ciclo { get; set; } 
+
+         [Required(ErrorMessage = "El campo es requerido")]
         public int Anio { get; set; }   
         [JsonIgnore]
         public virtual Materia? materia { get; set; } 
