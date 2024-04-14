3310ec49e0fb7e818fe389d66e8b8deb54c132f7
marcosherna <marcosxdf6@gmail.com>
2024-04-14 13:26:38 -0600

    obj, bin

--- obj/Debug/net8.0/guia-2.AssemblyInfo.cs
+++ obj/Debug/net8.0/guia-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guia-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f064dfd45aa99b923d261bdcb569755869a9e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56671bf7410066a392729ad2600bd617a8ad41e")]
 [assembly: System.Reflection.AssemblyProductAttribute("guia-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guia-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
